5bb81babb90b23ca561162d4ef118d331407fb06
yoyesu <yoyes@gmail.com>
2022-05-28 13:21:47 +0100

    TNB-3 general outline

---
+++ src/Home/components/WelcMess.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function WelcMess(){
+  return (
+    <div className="welc-mess">
+        <h3>Welcome to The New Bungalow</h3>
+        <p>As a family we have created lifelong memories across North Norfolk with this home at the heart. It has provided us with a secure and comfortable base to recoup and relax after exploring the coast and the countryside.</p>
+        <p>We are excited to now invite others to share in this and create their own memories of such a special place.</p>
+        <p>These pages will provide lots of useful information to help our guests make the most of their stay.</p>
+        <p>We hope you enjoy it as much as we have.</p>
+        <p>Sincerely,<br/>
+          The Cardys</p>
+
+    </div>
+  )
+}
