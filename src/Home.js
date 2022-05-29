aeaf4e01a5c2afa314e90e7ed3e9f3d72e69bcc9
yoyesu <yoyes@gmail.com>
2022-05-29 01:06:56 +0100

    TNB-35 image carousel on homepage

--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,15 @@ import "./home.css";
 import Weather from "./components/Weather";
 import WelcMess from "./components/WelcMess";
 import PicCarousel from "./components/PicCarousel";
-
+import { carouselData } from "./components/PicCarousel"
 export default function Home() {
   return (
     <div className="Home">
       {/* <Weather /> */}
       <div className="home-main">
-        <PicCarousel />
+        <PicCarousel 
+          slides={carouselData}
+        />
         <WelcMess />
       </div>
     </div>
--- src/components/PicCarousel.js
+++ src/components/PicCarousel.js
@@ -1,9 +1,51 @@
-import React from "react"
+import React, { useState } from "react"
+import CarouselImg1 from "../assets/outdoorDinning.jpg"
+import CarouselImg2 from "../assets/holkhambeach.png"
+import CarouselImg3 from "../assets/porch.jpg"
+import { FaArrowAltCircleRight } from "@react-icons/all-files/fa/FaArrowAltCircleRight"
+import { FaArrowAltCircleLeft } from "@react-icons/all-files/fa/FaArrowAltCircleLeft"
 
-export default function PicCarousel(){
+
+export const carouselData = [
+  {
+    image: CarouselImg1
+  },
+  {
+    image: CarouselImg2
+  },
+  {
+    image: CarouselImg3
+  }
+]
+
+
+export default function PicCarousel({slides}){
+  const [current, setCurrent] = useState(0)
+  const length = slides.length
+
+  const nextSlides = () => {
+    setCurrent(current === length -1 ? 0 : current +1)
+  }
+  const prevSlides = () => {
+    setCurrent(current === 0 ? length -1 : current -1)
+  }
+  if(!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
+
   return (
-    <img src="">
-    
-    </img>
+    <section className="carousel-container">
+      <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlides}/>
+      <FaArrowAltCircleRight className="right-arrow" onClick={nextSlides}/>
+      {carouselData.map((carousel, index) => {
+        return (
+          <div className={index === current ? 'carousel active' : 'carousel'} key={index}>
+            {index === current && (<img src={carousel.image} alt="carousel" className="images"></img>)}
+            
+          </div>
+          
+        )
+      })}
+    </section>
   )
 }
